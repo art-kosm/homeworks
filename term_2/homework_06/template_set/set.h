5f549baa63099151052de92023ddad616b0d77bc
art-kosm <kira666yagami@gmail.com>
2015-05-29 12:04:23 +0300

    functions are not inline anymore, constructor initialize list added

--- term_2/homework_06/template_set/set.h
+++ term_2/homework_06/template_set/set.h
@@ -4,17 +4,8 @@ template <typename T>
 class Set
 {
 public:
-    Set()
-    {
-        capacity = 3;
-        elementsNumber = 0;
-        contents = new T[capacity];
-    }
-
-    ~Set()
-    {
-        delete [] contents;
-    }
+    Set(int capacity = 3);
+    ~Set();
 
     /**
      * @brief Value adder
@@ -24,17 +15,7 @@ public:
      * @param value
      */
 
-    void add(T value)
-    {
-        if (!contains(value))
-        {
-            contents[elementsNumber] = value;
-            elementsNumber++;
-        }
-
-        if (elementsNumber == capacity)
-            extend();
-    }
+    void add(T value);
 
     /**
      * @brief Value remover
@@ -45,26 +26,9 @@ public:
      * @param value
      */
 
-    void remove(T value)
-    {
-        for (int i = 0; i < elementsNumber; i++)
-            if (contents[i] == value)
-            {
-                elementsNumber--;
-                for (int j = i; j < elementsNumber; j++)
-                    contents[j] = contents[j + 1];
-                break;
-            }
-    }
+    void remove(T value);
 
-    bool contains(T value) const
-    {
-        for (int i = 0; i < elementsNumber; i++)
-            if (contents[i] == value)
-                return true;
-
-        return false;
-    }
+    bool contains(T value) const;
 
     /**
      * @brief Sets intersecter
@@ -76,17 +40,7 @@ public:
      * @return intersection of sets
      */
 
-    static Set<T> *intersect(Set<T> *set1, Set<T> *set2)
-    {
-        Set<T> *intersected = new Set<T>;
-
-        for (int i = 0; i < set1->elementsNumber; i++)
-            for (int j = 0; j < set2->elementsNumber; j++)
-                if (set1->contents[i] == set2->contents[j])
-                    intersected->add(set1->contents[i]);
-
-        return intersected;
-    }
+    static Set<T> *intersect(Set<T> *set1, Set<T> *set2);
 
     /**
      * @brief Sets uniter
@@ -98,17 +52,7 @@ public:
      * @return union of sets
      */
 
-    static Set<T> *unite(Set<T> *set1, Set<T> *set2)
-    {
-        Set<T> *united = new Set<T>;
-
-        for (int i = 0; i < set1->elementsNumber; i++)
-            united->add(set1->contents[i]);
-        for (int i = 0; i < set2->elementsNumber; i++)
-            united->add(set2->contents[i]);
-
-        return united;
-    }
+    static Set<T> *unite(Set<T> *set1, Set<T> *set2);
 
 private:
     T *contents;
@@ -122,13 +66,95 @@ private:
      * Copies elements from contents array to the new one
      */
 
-    void extend()
-    {
-        int newCapacity = capacity * 3 / 2 + 1;
-        T *newContents = new T[newCapacity];
-        for (int i = 0; i < elementsNumber; i++)
-            newContents[i] = contents[i];
-        delete [] contents;
-        contents = newContents;
-    }
+    void extend();
 };
+
+
+
+template <typename T>
+Set<T>::Set(int capacity)
+{
+    this->capacity = capacity;
+
+    elementsNumber = 0;
+    contents = new T[capacity];
+}
+
+template <typename T>
+Set<T>::~Set()
+{
+    delete [] contents;
+}
+
+template <typename T>
+void Set<T>::add(T value)
+{
+    if (!contains(value))
+    {
+        contents[elementsNumber] = value;
+        elementsNumber++;
+    }
+
+    if (elementsNumber == capacity)
+        extend();
+}
+
+template <typename T>
+void Set<T>::remove(T value)
+{
+    for (int i = 0; i < elementsNumber; i++)
+        if (contents[i] == value)
+        {
+            elementsNumber--;
+            for (int j = i; j < elementsNumber; j++)
+                contents[j] = contents[j + 1];
+            break;
+        }
+}
+
+template <typename T>
+bool Set<T>::contains(T value) const
+{
+    for (int i = 0; i < elementsNumber; i++)
+        if (contents[i] == value)
+            return true;
+
+    return false;
+}
+
+template <typename T>
+Set<T> *Set<T>::intersect(Set<T> *set1, Set<T> *set2)
+{
+    Set<T> *intersected = new Set<T>;
+
+    for (int i = 0; i < set1->elementsNumber; i++)
+        for (int j = 0; j < set2->elementsNumber; j++)
+            if (set1->contents[i] == set2->contents[j])
+                intersected->add(set1->contents[i]);
+
+    return intersected;
+}
+
+template <typename T>
+Set<T> *Set<T>::unite(Set<T> *set1, Set<T> *set2)
+{
+    Set<T> *united = new Set<T>;
+
+    for (int i = 0; i < set1->elementsNumber; i++)
+        united->add(set1->contents[i]);
+    for (int i = 0; i < set2->elementsNumber; i++)
+        united->add(set2->contents[i]);
+
+    return united;
+}
+
+template <typename T>
+void Set<T>::extend()
+{
+    int newCapacity = capacity * 3 / 2 + 1;
+    T *newContents = new T[newCapacity];
+    for (int i = 0; i < elementsNumber; i++)
+        newContents[i] = contents[i];
+    delete [] contents;
+    contents = newContents;
+}
