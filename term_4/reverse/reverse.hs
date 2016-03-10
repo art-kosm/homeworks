f6aa9a001c795987d0f7b53d93ed807d1ee86fbe
art-kosm <kira666yagami@gmail.com>
2016-03-11 00:11:08 +0300

    tuples removed

--- term_4/reverse/reverse.hs
+++ term_4/reverse/reverse.hs
@@ -1,7 +1,7 @@
-match :: ([a], [a]) -> ([a], [a])
-match (list, [x]) = (x:list, [])
-match (x:xs, []) = match ([x], xs)
-match (y, x:xs) = match (x : y, xs)
+match :: [a] -> [a] -> [a]
+match list [x] = x:list
+match (x:xs) [] = match [x] xs
+match y (x:xs) = match (x:y) xs
 
 reverse' :: [a] -> [a]
-reverse' list = fst (match (list, []))
+reverse' list = match list []
