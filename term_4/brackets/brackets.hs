915953bb2cfde8d2b52538a5a96ccf57da9e8a2e
art-kosm <kira666yagami@gmail.com>
2016-07-06 22:01:49 +0300

    more bracket types

--- term_4/brackets/brackets.hs
+++ term_4/brackets/brackets.hs
@@ -1,10 +1,14 @@
 check_brackets :: String -> Bool
-check_brackets s = brackets s 0
+check_brackets = brackets "_"
 
-brackets :: String -> Int -> Bool
-brackets [] 0 = True
-brackets [] _ = False
-brackets (c:s) (-1) = False
-brackets (c:s) n | c == '('   = brackets s (n + 1)
-                 | c == ')'   = brackets s (n - 1)
-                 | otherwise  = brackets s n
+brackets :: String -> String -> Bool
+brackets "_" "" = True
+brackets  _  "" = False
+brackets (s:stack) (c:string) | c == '(' || c == '[' || c == '{'  = brackets (c:s:stack) string
+                              | (c == ')' && s == '(') || 
+                                (c == ']' && s == '[') || 
+                                (c == '}' && s == '{')             = brackets stack string
+                              | (c == ')' && s /= '(') || 
+                                (c == ']' && s /= '[') || 
+                                (c == '}' && s /= '{')             = False  
+                              | otherwise                          = brackets (s:stack) string
